8bbe2af6c5e04f6afda17c31be362dc9f9224bc7
Hossen <mdmitho199911@gmail.com>
2022-03-29 02:13:50 +0600

    nav link active route

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Home from './components/Home/Home'
 import Friends from './components/Friends/Friends'
 import About from './components/About/About'
 import NotFound from './components/NotFound/NotFound';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my fancy websit</h1>
+    <Header></Header>
        <Routes>
          <Route path='/' element={<Home/>}></Route>
          <Route path='/friends' element={<Friends></Friends>}></Route>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Customlink from '../CustomLink/Customlink';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+              <h1>Welcome to my fancy websit With Header Routing</h1>
+              <nav className='header'>
+                  <Customlink to="/">Home</Customlink>
+                  <Customlink to="/friends">Friends</Customlink>
+                  <Customlink to="/about">About</Customlink>
+              </nav>
+        </div>
+    );
+};
+
+export default Header;
