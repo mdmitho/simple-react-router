0ae00f628f5a137f86df615d781e73227e2cb526
Hossen <mdmitho199911@gmail.com>
2022-03-29 01:36:54 +0600

    load user data and Navigate dynamic

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const [friends , setFriends] = useState([])
+   
+    useEffect(()=> {
+       fetch('https://jsonplaceholder.typicode.com/users')
+       .then(res => res.json())
+       .then(data => setFriends(data))
+    },[])
     return (
         <div>
             <h2>Hello Friend . How are you</h2>
-            <p><small>Parle kicu taka dhar dis</small></p>
+            <p><small>Parle kicu taka dhar dis... : {friends.length}</small></p>
+            {
+                friends.map(friend => <Friend key={friend.id} friend={friend}></Friend>)
+            }
         </div>
     );
 };
